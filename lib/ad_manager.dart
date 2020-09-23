bc9e189dfa807402af34db893d71e2e652398346
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-09-23 15:44:36 +0100

    Create ad_manager.dart

---
+++ lib/ad_manager.dart
@@ -0,0 +1,6 @@
+class AdManager {
+
+  //change App and rewardIds here mr Ola.
+  static String appId = "ca-app-pub-3940256099942544~3347511713";
+  static String RewardId = "ca-app-pub-3940256099942544/5224354917";  
+}
