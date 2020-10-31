af7e1952069ffb752d22900f5c5ea53ff58086e4
olamilekan-adeleke <43634204+Ola66@users.noreply.github.com>
2020-10-31 15:33:41 +0100

    fix some more  bugs in food home page

--- lib/hostel_food/_/pages/food_home_page.dart
+++ lib/hostel_food/_/pages/food_home_page.dart
@@ -5,9 +5,11 @@ import 'package:Ohstel_app/hostel_food/_/models/fast_food_details_model.dart';
 import 'package:Ohstel_app/hostel_food/_/models/food_details_model.dart';
 import 'package:Ohstel_app/hostel_food/_/pages/selected_fast_food_page.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:paginate_firestore/paginate_firestore.dart';
 
 class FoodHomePage extends StatefulWidget {
   @override
@@ -16,11 +18,17 @@ class FoodHomePage extends StatefulWidget {
 
 class _FoodHomePageState extends State<FoodHomePage> {
   String uniName;
+  bool loading = true;
 
   Future<void> getUniName() async {
     String name = await HiveMethods().getUniName();
     print(name);
     uniName = name;
+
+    if (!mounted) return;
+    setState(() {
+      loading = false;
+    });
   }
 
   List<ItemDetails> getItemDetails(List data) {
@@ -44,14 +52,23 @@ class _FoodHomePageState extends State<FoodHomePage> {
   }
 
   @override
+  void initState() {
+    getUniName();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: ListView(
+            shrinkWrap: true,
             children: <Widget>[
               imageList(),
-              foodList(),
+              loading
+                  ? Center(child: CircularProgressIndicator())
+                  : paginatedFoodList(),
             ],
           ),
         ),
@@ -93,6 +110,135 @@ class _FoodHomePageState extends State<FoodHomePage> {
           );
         }).toList(),
       ),
+    );
+  }
+
+  Widget paginatedFoodList() {
+    return PaginateFirestore(
+      shrinkWrap: true,
+      initialLoader: Center(child: CircularProgressIndicator()),
+      emptyDisplay: Container(
+        child: Center(
+          child: Text('No Fast Food Found!'),
+        ),
+      ),
+      bottomLoader: Center(child: CircularProgressIndicator()),
+      query: FastFoodMethods()
+          .foodCollectionRef
+          .orderBy('fastFood')
+          .where('display', isEqualTo: true)
+          .where('uniName', isEqualTo: uniName),
+      itemBuilder:
+          (int index, BuildContext context, DocumentSnapshot snapshot) {
+        FastFoodModel currentFastFood = FastFoodModel.fromMap(snapshot.data());
+
+        List<ItemDetails> currentItemDetails =
+            getItemDetails(snapshot.data()['itemDetails']);
+
+        List<ExtraItemDetails> currentExtraItemDetails =
+            getExtraItemDetails(snapshot.data()['extraItems']);
+
+        return InkWell(
+          onTap: () {
+            print("currentFastFood.toMap():${currentFastFood.toMap()}");
+            print(
+                "currentItemDetails[0].toMap():${currentItemDetails[0].toMap()}");
+
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => SelectedFastFoodPage(
+                  currentFastFood: currentFastFood,
+                  currentExtraItemDetails: currentExtraItemDetails,
+                  currentItemDetails: currentItemDetails,
+                ),
+              ),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 5,
+              vertical: 5,
+            ),
+            child: Card(
+              color: Color(0xFFF4F5F6),
+              elevation: 1,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    height: 150,
+                    width: 150,
+                    child: ExtendedImage.network(
+                      currentFastFood.logoImageUrl,
+                      fit: BoxFit.fill,
+                      handleLoadingProgress: true,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(10),
+                      cache: false,
+                      enableMemoryCache: true,
+                    ),
+                  ),
+                  Container(
+                    height: 120,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${currentFastFood.fastFoodName.trim()}',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          '0801 345 6767',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          '${currentFastFood.openTime.trim()}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.black45,
+                              size: 14,
+                            ),
+                            Text(
+                              '${currentFastFood.address.trim()}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      itemBuilderType: PaginateBuilderType.listView,
     );
   }
 
